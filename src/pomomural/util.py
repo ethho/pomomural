567d9407c476551d9a9a0db0da141a6a3dd36aeb
Ajay-26 <64776385+Ajay-26@users.noreply.github.com>
2023-03-25 23:18:47 -0500

    update to change hours to just minutes

--- src/pomomural/util.py
+++ src/pomomural/util.py
@@ -2,4 +2,4 @@ def get_time_from_seconds(seconds:float)->str:
 	mins = int(seconds)//60
 	hours = mins//60
 	mins_to_hour = mins%60
-	return f"Time taken to reach: {hours:02d}:{mins_to_hour:02d}"
+	return f"Time taken to reach: {mins_to_hour:02d} mins"
